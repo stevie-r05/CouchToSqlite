54bc02cd97f669b374b0dc927635d3000c0bca81
Stevie Robinson <srrobin4@asu.edu>
2012-11-12 15:59:33 -0700

    Added parsing for Json keys

--- Eclipse Project/src/edu/asu/beastd/Parse.java
+++ Eclipse Project/src/edu/asu/beastd/Parse.java
@@ -14,5 +14,12 @@ public class Parse {
 		JSONObject jsonObject = form.JsonParsing(filepath);
 		System.out.println(jsonObject.toJSONString());
 		scan.close();
+		
+		Object[] keySetObj = jsonObject.keySet().toArray();
+		String[] keySet = new String[keySetObj.length];
+		for(int i=0;i<keySetObj.length;i++){
+			keySet[i] = keySetObj[i].toString();
+			System.out.println(keySet[i]);
+		}
 	}
 }
