75dedd3a99432ba2128803b8a25aa0cdd011f241
edalford11 <eric.alford@asu.edu>
2012-11-12 14:35:06 -0700

    SQLite DB access and modification

---
+++ Eclipse Project/src/edu/asu/beastd/SQLite.java
@@ -0,0 +1,41 @@
+import java.sql.Connection;  
+import java.sql.DriverManager;  
+import java.sql.ResultSet;  
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class SQLite {
+	Connection connection;  
+    ResultSet resultSet;  
+    Statement statement;  
+
+	public SQLite(String databse){
+		resultSet = null;
+		statement = null;
+        try{
+        	Class.forName("org.sqlite.JDBC");
+        	connection = DriverManager.getConnection("jdbc:sqlite:" + databse);
+        } catch (Exception e) {  
+            e.printStackTrace();  
+        }
+	}
+	
+	public void insertQuery(String queryStatement){
+		try {
+			statement = connection.createStatement();
+			statement.executeQuery(queryStatement);
+			resultSet.close();  
+            statement.close(); 
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}  
+	}
+	
+	public void closeDatabase(){
+		 try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
