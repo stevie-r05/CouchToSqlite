59b7518142f8e9ea4ba5444c2b36f00157e28cea
Stevie Robinson <srrobin4@asu.edu>
2012-11-12 18:32:10 -0700

    Completed conversion to SQLite tables
    Next task is to make table creation with dynamic naming.

--- Eclipse Project/src/edu/asu/beastd/SQLite.java
+++ Eclipse Project/src/edu/asu/beastd/SQLite.java
@@ -6,33 +6,52 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * SQLite is the class used for bridging to a SQLite file.
+ * It provides the commands for actually executing SQL statements.
+ * @author BEASTD
+ */
 public class SQLite {
 	Connection connection;  
     ResultSet resultSet;  
     Statement statement;  
 
-	public SQLite(String databse){
+    /**
+     * Initializes connection.
+     * @param database The location of the database to be edited. Created if it doesn't exist.
+     * @author BEASTD
+     */
+	public SQLite(String database){
 		resultSet = null;
 		statement = null;
         try{
         	Class.forName("org.sqlite.JDBC");
-        	connection = DriverManager.getConnection("jdbc:sqlite:" + databse);
+        	connection = DriverManager.getConnection("jdbc:sqlite:" + database);
         } catch (Exception e) {  
             e.printStackTrace();  
         }
 	}
 	
-	public void insertQuery(String queryStatement){
+	/**
+	 * Executes any SQL statement.
+	 * @param queryStatement The SQL statement to be executed.
+	 * @author BEASTD
+	 */
+	public void executeSql(String queryStatement){
 		try {
 			statement = connection.createStatement();
-			statement.executeQuery(queryStatement);
-			resultSet.close();  
+			statement.execute(queryStatement);
             statement.close(); 
 		} catch (SQLException e) {
+			// add log?
 			e.printStackTrace();
 		}  
 	}
 	
+	/**
+	 * Closes DB connection.
+	 * @author BEASTD
+	 */
 	public void closeDatabase(){
 		 try {
 			connection.close();
