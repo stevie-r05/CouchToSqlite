69f978a19d57291f4d7b6f2ebc17c905e50d3844
Stevie Robinson <wafflecopter05@gmail.com>
2012-12-30 17:52:29 -0700

    Performed some componentization of conversion
    Also changed dates for copyright and performed other refactoring bits

--- src/edu/asu/beastd/CouchToSqlite.java
+++ src/edu/asu/beastd/CouchToSqlite.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 Stevie Robinson, Eric Alford, Tara Mendoza, Blake Tucker, Anthony Sanchez, Davenn Mannix
+// Copyright (C) 2012-2013 Stevie Robinson, Eric Alford, Tara Mendoza, Blake Tucker, Anthony Sanchez, Davenn Mannix
 //
 // This file is part of CouchToSqlite.
 //
@@ -21,7 +21,6 @@ import com.fourspaces.couchdb.*;
 import com.sun.istack.internal.logging.Logger;
 
 import java.io.*;
-import java.sql.SQLException;
 import java.util.*;
 import java.util.logging.Level;
 
@@ -36,9 +35,6 @@ public class CouchToSqlite {
 	
 	private static Logger LOG = Logger.getLogger(CouchToSqlite.class);
 	
-	private String dbPath;
-	private String couchHost;
-	private int couchPort;
 	private SQLite sqlite;
 	private Session couchSession;
 	
@@ -80,9 +76,6 @@ public class CouchToSqlite {
 	 * @throws SqliteException If there is an error accessing the SQLite .db location
 	 */
 	public CouchToSqlite(String dbPath, String couchHost, int couchPort) throws CouchException, SqliteException {
-		this.dbPath = dbPath;
-		this.couchHost = couchHost;
-		this.couchPort = couchPort;
 		try {
 			this.sqlite = new SQLite(dbPath);
 		} catch (Exception e){
@@ -107,119 +100,149 @@ public class CouchToSqlite {
 		List<String> databaseList = null;
 		try
 		{
-			databaseList = couchSession.getDatabaseNames();
+			databaseList = getDatabaseNames();
 		} catch (Exception e) {
 			LOG.logException(e, Level.WARNING);
 			throw new CouchException("Problem getting database names from Couch.", e.getCause());
 		}
-		
-		// Verifies the database names are retrieved correctly (for debug)
-			for(int i=0;i<databaseList.size();i++)
-			{
-				System.out.println(couchSession.getDatabase(databaseList.get(i)).getName());
+			
+		// For each db in databaseList, get the documents, create a table for each db, add in documents to each table.
+		// TODO: Add in transaction handling using JDBC. Only commit transaction after we've finished this for loop?
+		for(int i=0;i<databaseList.size();i++)
+		{
+			// Acquire each database and get its documents
+			Database db = couchSession.getDatabase(databaseList.get(i));
+			ViewResults docList = db.getAllDocuments();
+			
+			// Initialize table structure using the first document
+			if (!(docList.getResults().isEmpty())) {
+				try {
+					initializeTable(db.getName(), db.getDocument(docList.getResults().get(0).getId()));
+				} catch (IOException e) {
+					LOG.logException(e, Level.WARNING);
+					throw new CouchException("Problem getting CouchDB through couchdb4j.", e.getCause());
+				}
 			}
 			
-			/* For each db in databaseList, get the documents, create a table for each db, add in
-			 * documents to each table. 
-			 * TODO: Split this further into methods/classes
-			 */
-			for(int i=0;i<databaseList.size();i++)
+			// Now, add in the rest of the documents
+			if (docList.size() > 1)
 			{
-				// Acquire each database and get its documents
-				Database db = couchSession.getDatabase(databaseList.get(i));
-				ViewResults docList = db.getAllDocuments();
-				
-				// Get first document to initialize the table structure. docFields holds the column values.
-				// fieldIt allows us to iterate through each field in order.
-				Document doc;
-				if (!(docList.getResults().isEmpty())) {
-					try {
-						doc = db.getDocument(docList.getResults().get(0).getId());
-					} catch (IOException e) {
-						throw new CouchException("Problem accessing CouchDB document.", e.getCause());
-					}
-					Set<String> docFields = doc.keySet();
-					Iterator<String> fieldIt = docFields.iterator();
-					
-					// Drops the table if it exists. Ignores otherwise.
-					try{
-						sqlite.executeSql("DROP TABLE " + db.getName());
-					}
-					catch (Exception e) {
-						LOG.info("Skipped dropping table " + db.getName(), e);
-					}
-					
-					// Creates the table for the particular database. Initializes all columns to the structure of the
-					// first document.
-					// TODO: Somebody add in type checking, so we don't only add in strings.
-					sqlite.executeSql("CREATE TABLE " + db.getName() + "(" + fieldIt.next() + " VARCHAR(100000000))");
-					while (fieldIt.hasNext())
-					{
-						sqlite.executeSql("ALTER TABLE " + db.getName() + " ADD COLUMN " + fieldIt.next() + " VARCHAR(100000000)");
-					}
-					
-					fieldIt = docFields.iterator(); // reset fieldIt for adding values into doc.
-					
-					// Build keySetString and valueSetString to inject into SQLite command.
-					// We do this by moving through the fields and adding values to the string
-					String keySetString = new String();
-					while(fieldIt.hasNext())
-					{
-						keySetString += fieldIt.next() + ",";
-					}
-					keySetString = keySetString.substring(0, keySetString.length()-1); //remove extra comma
-					fieldIt = docFields.iterator();
-					String valueSetString = new String();
-					while(fieldIt.hasNext())
-					{
-						// replaceAll is to add escape character for single quote
-						valueSetString += "'" + doc.getString(fieldIt.next()).replaceAll("'", "''") + "',";
-					}
-					valueSetString = valueSetString.substring(0, valueSetString.length()-1);
-					sqlite.executeSql("INSERT INTO " + db.getName() + "( " + keySetString + ") VALUES (" + valueSetString + ")");
-				}
-				
-				// Now, move through each document in a particular DB to add more rows
-				// TODO: Merge in with table creation to avoid redundant code
-				for(int j=1;j<docList.getResults().size();j++)
-				{
-					try {
-						doc = db.getDocument(docList.getResults().get(j).getId());
-					} catch (IOException e) {
-						LOG.logException(e, Level.WARNING);
-						throw new CouchException("Problem accessing CouchDB document.", e.getCause());
-					}
-					Set<String> docFields = doc.keySet();
-					Iterator<String> fieldIt = docFields.iterator();
-					String keySetString = "";
-					while(fieldIt.hasNext())
-					{
-						keySetString += fieldIt.next() + ",";
-					}
-					keySetString = keySetString.substring(0, keySetString.length()-1);
-					fieldIt = docFields.iterator();
-					String valueSetString = "";
-					while(fieldIt.hasNext())
-					{
-						// Checks if a particular column already exists. If it doesn't, it is added to
-						// the appropriate table.
-						String columnName = fieldIt.next();
-						try {
-							if (!(sqlite.hasColumn(columnName, db.getName())))
-							{
-								sqlite.executeSql("ALTER TABLE " + db.getName() + " ADD COLUMN " + columnName + " VARCHAR(100000000)");
-							}
-						} catch (SqliteException e) {
-							LOG.logException(e, Level.WARNING);
-							throw new SqliteException("Error adding new column to a table: " + e.getMessage(), e.getCause());
-						}
-						valueSetString += "'" + doc.getString(columnName).replaceAll("'", "''") + "',";
-					}
-					valueSetString = valueSetString.substring(0, valueSetString.length()-1);
-					sqlite.executeSql("INSERT INTO " + db.getName() + "( " + keySetString + ") VALUES (" + valueSetString + ")");
-				} //end docList for loop
-				
-			} //end db for loop
+				addDocumentsToTable(db.getName(), docList.getResults().subList(1, docList.getResults().size()));
+			}
+			
+		} //end db for loop
 	}
+	
+	/**
+	 * Retrieves the list of database names from the couch session.
+	 * @return A list of strings containing the names of all databases in the instance
+	 * @throws CouchException If there is any error acquiring the database names from Couch
+	 */
+	public List<String> getDatabaseNames() throws CouchException{
+		List<String> toReturn;
+		try {
+			toReturn = couchSession.getDatabaseNames();
+		} catch (Exception e) {
+			LOG.logException(e, Level.WARNING);
+			throw new CouchException("Problem getting database names from Couch.", e.getCause());
+		}
+		return toReturn;
+	}
+	
+	/**
+	 * Method to initialize a SQLite table for a particular database
+	 * @param databaseName The CouchDB database name (used as table name)
+	 * @param doc The initial document used to structure the SQLite table
+	 * @throws SqliteException If there are any SQLite-related errors
+	 */
+	private void initializeTable(String databaseName, Document doc) throws SqliteException {
+		
+		@SuppressWarnings("unchecked") // We can add this because couchdb4j stores key-value pairs as strings
+		Set<String> docFields = doc.keySet();
+		
+		Iterator<String> fieldIt = docFields.iterator();
+		
+		// Drops the table if it exists. Ignores otherwise.
+		try{
+			sqlite.executeSql("DROP TABLE " + databaseName);
+		}
+		catch (Exception e) {
+			LOG.info("Skipped dropping table " + databaseName, e);
+		}
+		
+		// Creates the table for the particular database. Initializes all columns to the structure of the
+		// first document.
+		// TODO: Somebody add in type checking, so we don't only add in strings.
+		sqlite.executeSql("CREATE TABLE " + databaseName + "(" + fieldIt.next() + " VARCHAR(100000000))");
+		while (fieldIt.hasNext())
+		{
+			sqlite.executeSql("ALTER TABLE " + databaseName + " ADD COLUMN " + fieldIt.next() + " VARCHAR(100000000)");
+		}
+		
+		fieldIt = docFields.iterator(); // reset fieldIt for adding values into doc.
+		
+		// Build keySetString and valueSetString to inject into SQLite command.
+		// We do this by moving through the fields and adding values to the string
+		String keySetString = new String();
+		while(fieldIt.hasNext())
+		{
+			keySetString += fieldIt.next() + ",";
+		}
+		keySetString = keySetString.substring(0, keySetString.length()-1); //remove extra comma
+		fieldIt = docFields.iterator();
+		String valueSetString = new String();
+		while(fieldIt.hasNext())
+		{
+			// replaceAll is to add escape character for single quote
+			valueSetString += "'" + doc.getString(fieldIt.next()).replaceAll("'", "''") + "',";
+		}
+		valueSetString = valueSetString.substring(0, valueSetString.length()-1);
+		sqlite.executeSql("INSERT INTO " + databaseName + "( " + keySetString + ") VALUES (" + valueSetString + ")");
+	}
+	
+	/**
+	 * Auxiliary method used to add additional documents to a particular table after it has already been initialized.
+	 * @param tableName The table to add rows to
+	 * @param docList The list of documents to add to said table (the table should already have been initialized using initializeTable)
+	 * @throws SqliteException If there are any SQLite-related errors
+	 */
+	private void addDocumentsToTable(String tableName, List<Document> docList) throws SqliteException {
+		for(int i=0;i<docList.size();i++){
+			Document doc = docList.get(i);
+			
+			@SuppressWarnings("unchecked") // We can add this because couchdb4j stores key-value pairs as strings
+			Set<String> docFields = doc.keySet();
+			
+			Iterator<String> fieldIt = docFields.iterator();
+			String keySetString = "";
+			while(fieldIt.hasNext())
+			{
+				keySetString += fieldIt.next() + ",";
+			}
+			keySetString = keySetString.substring(0, keySetString.length()-1);
+			fieldIt = docFields.iterator();
+			String valueSetString = "";
+			while(fieldIt.hasNext())
+			{
+				// Checks if a particular column already exists. If it doesn't, it is added to
+				// the appropriate table.
+				String columnName = fieldIt.next();
+				try {
+					if (!(sqlite.hasColumn(columnName, tableName)))
+					{
+						sqlite.executeSql("ALTER TABLE " + tableName + " ADD COLUMN " + columnName + " VARCHAR(100000000)");
+					}
+				} catch (SqliteException e) {
+					LOG.logException(e, Level.WARNING);
+					throw new SqliteException("Error adding new column to a table: " + e.getMessage(), e.getCause());
+				}
+				valueSetString += "'" + doc.getString(columnName).replaceAll("'", "''") + "',";
+			}
+			valueSetString = valueSetString.substring(0, valueSetString.length()-1);
+			sqlite.executeSql("INSERT INTO " + tableName + "( " + keySetString + ") VALUES (" + valueSetString + ")");
+		}
+	}
+	
+	
 
 }
--- src/edu/asu/beastd/SQLite.java
+++ src/edu/asu/beastd/SQLite.java
@@ -1,4 +1,4 @@
-// Copyright (C) 2012 Stevie Robinson, Eric Alford, Tara Mendoza, Blake Tucker, Anthony Sanchez, Davenn Mannix
+// Copyright (C) 2012-2013 Stevie Robinson, Eric Alford, Tara Mendoza, Blake Tucker, Anthony Sanchez, Davenn Mannix
 //
 // This file is part of CouchToSqlite.
 //
